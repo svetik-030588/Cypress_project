27ed3166ccc75a1015ca434fd3506026275f262a
Родионова Светлана <my_new_mail2014@mail.ru>
2024-03-10 11:24:34 +0300

    change

--- cypress/e2e/Test2.cy.js
+++ cypress/e2e/Test2.cy.js
@@ -1,1 +1,16 @@
 ///<reference types="Cypress" />
+it('' , () => {
+    cy.visit('https://msk.tele2.ru/payments/refill')
+    cy.contains('Войти')
+})
+
+it('' , () => {
+    cy.visit('https://msk.tele2.ru/payments/refill')
+    cy.contains('span', 'Войти')
+})
+
+//нерабочий, нет примера
+it('' , () => {
+    cy.visit('https://docs.cypress.io/guides/overview/why-cypress')
+    cy.get('footer').contains('theme-last-updated');   //get проигнорировван из-за иерархии
+})
