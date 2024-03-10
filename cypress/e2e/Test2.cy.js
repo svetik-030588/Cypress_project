ddf4d30ec39815e44b378ab5443e053139cc9da4
Родионова Светлана <my_new_mail2014@mail.ru>
2024-03-10 15:14:16 +0300

    change

--- cypress/e2e/Test2.cy.js
+++ cypress/e2e/Test2.cy.js
@@ -1,1 +1,22 @@
 ///<reference types="Cypress" />
+
+//const { should } = require("chai")
+
+
+it('SHOULD', () => {
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/account')
+    cy.get('[ng-model]')
+    .type(100)
+    .should('have.value', 11)
+    .and('be.visible')
+
+})
+
+it.only('EXPECT', () => {
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/account')
+    cy.get('[ng-model]')
+    .type(100).then( input => {
+        expect(input).to.have.value(100)
+    }
+    )
+
